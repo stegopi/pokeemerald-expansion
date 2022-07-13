808ad34433f1c3261962e04c0aadd35c011da7e0
Shu Yan <austintexasnorthamerica@gmail.com>
2022-07-13 11:51:05 -0700

    Rename newmap 1

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1057,6 +1057,8 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 
-	.include "data/maps/NewMap1/scripts.inc"
+	.include "data/maps/Wilderdeen_Clearing/scripts.inc"
 
 	.include "data/maps/NewMap2/scripts.inc"
+
+	.include "data/maps/NewMap3/scripts.inc"
