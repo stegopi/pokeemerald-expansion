576cb40d0ba9997e3044c0c514972bcf80523a00
Austin Shu <austintexasnorthamerica@gmail.com>
2022-08-12 12:40:49 -0700

    Finish port of Stego den lava

--- randomsecondarycaveicedraw.js
+++ randomsecondarycaveicedraw.js
@@ -3,12 +3,17 @@ function randInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-const iceTiles = [0x38C, 0x386, 0x387, 0x38F, 0x38E, 0x385, 0x381, 0x28F, 0x381, 0x381, 0x381, 0x381];
-
-export function onBlockChanged(x, y, prevBlock, newBlock) {
-    if (iceTiles.indexOf(newBlock.metatileId) != -1 && map.getSecondaryTileset() == "gTileset_Cave") {
-        const i = randInt(0, iceTiles.length);
-        map.setMetatileId(x, y, iceTiles[i]);
+function IceCavetiles() {
+    export function onBlockChanged(x, y, prevBlock, newBlock) {
+        if (iceTiles.indexOf(newBlock.metatileId) != -1 && map.getSecondaryTileset() == "gTileset_Cave") {
+            const i = randInt(0, iceTiles.length);
+            map.setMetatileId(x, y, iceTiles[i]);
+        }
     }
 }
+const iceTiles = [0x38C, 0x386, 0x387, 0x38F, 0x38E, 0x385, 0x381, 0x28F, 0x381, 0x381, 0x381, 0x381];
+
+export function onProjectOpened( ) {
+    map.registerAction("IceCavetiles", "Enable Ice Random", "Ctrl+Shift+I")
+}
+
