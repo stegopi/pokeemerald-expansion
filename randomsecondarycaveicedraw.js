efcb455c44dc3281971f31f2c41992dc23b82b59
Austin Shu <austintexasnorthamerica@gmail.com>
2022-08-16 20:47:06 -0700

    Use Colored headbutt

--- randomsecondarycaveicedraw.js
+++ randomsecondarycaveicedraw.js
@@ -3,17 +3,12 @@ function randInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function IceCavetiles() {
-    export function onBlockChanged(x, y, prevBlock, newBlock) {
-        if (iceTiles.indexOf(newBlock.metatileId) != -1 && map.getSecondaryTileset() == "gTileset_Cave") {
-            const i = randInt(0, iceTiles.length);
-            map.setMetatileId(x, y, iceTiles[i]);
-        }
-    }
-}
+
 const iceTiles = [0x38C, 0x386, 0x387, 0x38F, 0x38E, 0x385, 0x381, 0x28F, 0x381, 0x381, 0x381, 0x381];
 
-export function onProjectOpened( ) {
-    map.registerAction("IceCavetiles", "Enable Ice Random", "Ctrl+Shift+I")
+export function onBlockChanged(x, y, prevBlock, newBlock) {
+    if (iceTiles.indexOf(newBlock.metatileId) != -1 && map.getSecondaryTileset() == "gTileset_Cave") {
+        const i = randInt(0, iceTiles.length);
+        map.setMetatileId(x, y, iceTiles[i]);
+    }
 }
-
