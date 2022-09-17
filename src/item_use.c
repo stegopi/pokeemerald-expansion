274f808881856d2e29498eff3f851b22c4c33e83
Austin Shu <austintexasnorthamerica@gmail.com>
2022-09-17 10:33:32 -0700

    Fix  lava fishing and ffrost canyon south arrow

--- src/item_use.c
+++ src/item_use.c
@@ -268,7 +268,7 @@ static bool32 CanFish(void)
 
     if (!TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_SURFING))
     {
-        if (IsPlayerFacingSurfableFishableWater())
+        if (IsPlayerFacingSurfableFishableWater() && !MetatileBehavior_IsLavaSurf(tileBehavior))
             return TRUE;
     }
     else
