8be85481be178e8d49a8111e72628de87edc6629
Austin Shu <austintexasnorthamerica@gmail.com>
2022-09-28 20:39:37 -0700

    rename berries

--- include/constants/berry.h
+++ include/constants/berry.h
@@ -35,11 +35,11 @@
 // IDs for berry tree objects, indexes into berryTrees in SaveBlock1
 // Named for whatever berry is initially planted there on a new game
 // Those with no initial berry are named "soil"
-#define BERRY_TREE_ROUTE_102_PECHA    1
-#define BERRY_TREE_ROUTE_102_ORAN     2
+#define BERRY_TREE_ROUTE_2_CHESTO     1
+#define BERRY_TREE_ROUTE_2_ORAN       2
 #define BERRY_TREE_ROUTE_104_SOIL_1   3
 #define BERRY_TREE_ROUTE_104_ORAN_1   4
-#define BERRY_TREE_ROUTE_103_CHERI_1  5
+#define BERRY_TREE_ROUTE_2_CHERI  5
 #define BERRY_TREE_ROUTE_103_LEPPA    6
 #define BERRY_TREE_ROUTE_103_CHERI_2  7
 #define BERRY_TREE_ROUTE_104_CHERI_1  8
